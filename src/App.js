c8d2603919e645a3e5dc05f28a2a12c88a6040d7
Mariana Galan <gatm870125@gmail.com>
2021-02-13 13:51:33 -0600

    form with validation error message

--- src/App.js
+++ src/App.js
@@ -3,14 +3,17 @@ import './App.css';
 import { useForm } from "react-hook-form";
 
 export default function App() {
-  const { register, handleSubmit} = useForm();
+  const { register, handleSubmit, errors} = useForm();
   const onSubmit = data => console.log(data);
 
   return (
     <form onSubmit={handleSubmit(onSubmit)}>
       <input placeholder="Nick" name="nick" ref={register ({ required: true})} />
+      {errors.nick && errors.nick.type === "required" && <p>Este campo es obligatorio</p>}
       <input placeholder="Correo electrónico" name="email" ref={register ({ required: true})} />
+      {errors.email && errors.email.type === "required" &&<p>Este campo es obligatorio</p>}
       <input placeholder="Contraseña" name="password" ref={register ({ required: true})} />
+      {errors.password && errors.password.type === "required" &&<p>Este campo es obligatorio</p>}
       <input type="submit" />
     </form>
   );
