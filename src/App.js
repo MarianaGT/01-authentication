4a820bec2b687bd66e751450a34961747ca65dd1
Mariana Galan <gatm870125@gmail.com>
2021-02-13 13:38:25 -0600

    react hook form install

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
 import logo from './logo.svg';
 import './App.css';
+import { useForm } from "react-hook-form";
 
-function App() {
+export default function App() {
+  const { register, handleSubmit} = useForm();
+  const onSubmit = data => console.log(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <input placeholder="Nick" name="nick" ref={register} />
+      <input placeholder="Correo electrónico" name="email" ref={register} />
+      <input placeholder="Contraseña" name="password" ref={register} />
+      <input type="submit" />
+    </form>
   );
 }
-
-export default App;
